fa19f7d48244f953a44703aadb46f82002356117
kmr5326 <kmr5326@naver.com>
2025-09-24 15:25:42 +0900

    분산 추적 zipkin

--- com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -1,13 +1,11 @@
 package com.spring_cloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -15,11 +13,8 @@ public class ProductController {
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
 
-    @Value("${message}")
-    private String message;
-
-    @GetMapping
-    public String getProduct() {
-        return "Product detail from PORT : " + serverPort + " and message : " + this.message ;
+    @GetMapping("/{productId}")
+    public String getProduct(@PathVariable String productId) {
+        return "Product detail from PORT : " + serverPort;
     }
 }
