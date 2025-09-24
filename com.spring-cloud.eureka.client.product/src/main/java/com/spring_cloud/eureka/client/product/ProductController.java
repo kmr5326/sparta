56c391c3bf022949ecc16b6fd79bf4bc98b1fda3
kmr5326 <kmr5326@naver.com>
2025-09-24 14:30:16 +0900

    spring cloud config

--- com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -1,18 +1,25 @@
 package com.spring_cloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
-    @Value("${server.port}")
+    @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
 
-    @GetMapping("/product")
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
     public String getProduct() {
-        return "Product info!!!!! from port : " + serverPort;
+        return "Product detail from PORT : " + serverPort + " and message : " + this.message ;
     }
 }
